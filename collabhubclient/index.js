1d1bfa238a8eba3a564a41979cedd7bf0b0fad7d
Nick <nickthwang@gmail.com>
2021-04-06 09:02:24 -0500

    Update index.js

--- collabhubclient/index.js
+++ collabhubclient/index.js
@@ -1,7 +1,13 @@
 // check client version (4.0.1)
-const io = require("socket.io-client")
+const io = require("socket.io-client");
 //   EventEmitter = require("events");
 // Events = new EventEmitter();
+
+MESSAGETYPE = {
+  EVENT: "event",
+  CONTROL: "control",
+  CHAT: "chat",
+};
 
 ENVIRONMENT = {
   ARDUINO: "arduino",
@@ -38,12 +44,11 @@ Collabclient = class Collabclient {
 
     // env specific
     if (this.environment == ENVIRONMENT.PD) {
-
       //#region PD
       this.client = new PDClient({
-        name : "PD", 
-        socket : this.socket, 
-        toClientMethod: this.toClient
+        name: "PD",
+        socket: this.socket,
+        toClientMethod: this.toClient,
       });
     }
 
@@ -80,8 +85,6 @@ Collabclient = class Collabclient {
       let myRoomsUmenu = {
         items: data,
       };
-      //   max.outlet("myRoomsView", myRoomsView);
-      //   max.outlet("myRoomsUmenu", myRoomsUmenu);
     });
 
     this.socket.on("allRooms", (data) => {
@@ -91,14 +94,9 @@ Collabclient = class Collabclient {
       let allRoomsUmenu = {
         items: data,
       };
-      //   max.outlet("allRoomsView", allRoomsView);
-      //   max.outlet("allRoomsUmenu", allRoomsUmenu);
     });
 
-    this.socket.on("allRoomDetails", (data) => {
-      //   max.outlet("allRoomDetailsView", data);
-      //   max.outlet("allRoomDetailsUmenu", data);
-    });
+    this.socket.on("allRoomDetails", (data) => {});
 
     this.socket.on("users", (data) => {
       let usersView = {
@@ -107,14 +105,13 @@ Collabclient = class Collabclient {
       let usersUmenu = {
         items: data,
       };
-      //   max.outlet("usersView", usersView);
-      //   max.outlet("usersUmenu", usersUmenu);
     });
 
-    this.socket.on("event", (header) => {
-      console.log(`Event occured! ${header}`);
+    this.socket.on("event", (data) => {
+      console.log(`Event occured! ${data.header}`);
       // parse for PD
-      this.clientOut.send(header.header, 0, header.header.length);
+      this.client.toEnv(MESSAGETYPE.EVENT, data);
+      // this.clientOut.send(header.header, 0, header.header.length);
       // this.clientOut.send(header, 0, header.length);
     });
 
@@ -129,23 +126,21 @@ Collabclient = class Collabclient {
     });
 
     // Data from server
-    this.socket.on("control", (...incoming) => {
-      //   max.outlet("control", incoming);
+    this.socket.on("control", (data) => {
+      this.client.toEnv(MESSAGETYPE.CONTROL, data);
     });
 
-    this.socket.on("event", (incoming) => {
-      //   max.outlet("event", incoming);
-    });
-
-    this.socket.on("chat", (incoming) => {
+    this.socket.on("chat", (data) => {
       //   max.outlet("chat", incoming);
     });
 
     // --------------------
   }
 
-  toClient (routing, data) {
+  toClient(routing, data) {
     console.log(`client received data: ${routing}, ${data}`);
+    console.dir(data);
+    this.socket.emit(routing, data);
   }
 
   GetSocketID() {
@@ -159,10 +154,9 @@ Collabclient = class Collabclient {
   }
 };
 
-
 // code specifically for PD
 class PDClient {
-  constructor (options){
+  constructor(options) {
     const dgram = require("dgram");
     const receiver = dgram.createSocket("udp4");
     const sender = dgram.createSocket("udp4");
@@ -174,81 +168,115 @@ class PDClient {
     this.socket = options.socket;
 
     this.toClient = options.toClientMethod;
-    console.log('what is toclient: ' + this.toClient);
+    console.log("what is toclient: " + this.toClient);
+
+    this.clientOut = dgram.createSocket("udp4");
+    this.clientOut.connect(this.sendPort, "localhost");
 
     receiver.bind({
       address: "localhost",
-      port: 3002,
+      port: this.recPort,
       exclusive: true,
     });
 
+    // setup listening port from PD
+    receiver.on("listening", () => {
+      const address = receiver.address();
+      console.log(`CH-Cient listening at ${address.address}:${address.port}`);
+    });
 
-      // setup listening port from PD
-      receiver.on("listening", () => {
-        const address = receiver.address();
-        console.log(`CH-Cient listening at ${address.address}:${address.port}`);
-      });
+    // setup Event routing
+    receiver.on("message", (msg, rinfo) => {
+      console.log(
+        `CH-Client received MESSAGE: ${msg} from ${rinfo.address}:${rinfo.port}`
+      );
+      console.log(typeof msg);
+      msg = msg
+        .toString("utf8")
+        .substring(0, msg.length - 2)
+        .split(" ");
+      console.log("msg: " + msg);
 
-      // setup Event routing
-      receiver.on("message", (msg, rinfo) => {
-        console.log(
-          `CH-Client received MESSAGE: ${msg} from ${rinfo.address}:${rinfo.port}`
-        );
-        console.log(typeof msg);
-        msg = msg
-          .toString("utf8")
-          .substring(0, msg.length - 2)
-          .split(" ");
-        console.log(msg);
+      if (Array.isArray(msg)) {
+        console.log("msg len:" + msg.length);
+        let outgoing = {};
 
-        if (Array.isArray(msg)) {
-          console.log("msg len:" + msg.length);
-          if (msg[0] === "push") {
-            let outgoing = { mode: "push" };
-            if (msg.length > 3) {
-              outgoing.target = msg[1];
-              outgoing.header = msg[2];
-              this.toClient("control", {outgoing});
-            } else {
-              outgoing.target = msg[1];
-              outgoing.header = msg[2];
-              this.toClient("control", {outgoing});
-              // this.socket.emit("control", outgoing);
-            }
+        if (msg[0] === "push") {
+          outgoing.mode = "push";
+          if (msg.length > 3) {
+            outgoing.target = msg[1];
+            outgoing.header = msg[2];
+            outgoing.values = msg.slice(3);
+            this.toClient("control", outgoing);
+          } else {
+            outgoing.target = msg[1];
+            outgoing.header = msg[2];
+            this.toClient("event", outgoing);
+            // this.socket.emit("control", outgoing);
           }
         }
-      });
 
-      const message = String(
-        `Connected to Collab-Hub Client! You are using ${this.environment}. Send messages to Port ${this.recPort}`
-      );
-      this.clientOut = dgram.createSocket("udp4");
-      this.clientOut.connect(this.sendPort, "localhost", (err) => {
-        console.log("sending message...");
-        this.clientOut.send(message, (err) => {
-          // this.clientOut.close();
-        });
-        // this.clientOut.send(message, 0, message.length);
-      });
+        if (msg[0] === "chat") {
+          msg.splice(0, 1);
+          console.log("chat?");
+          let newMsg = msg.join(" ");
+          console.dir(newMsg);
+          outgoing.chat = newMsg;
+          outgoing.target = "all";
+          this.toClient("chat", outgoing);
+        }
 
+        if (msg[0] === "addUsername") {
+          outgoing.username = msg[1];
+          this.toClient("addUsername", outgoing);
+        }
+
+        if (msg[0] === "observeAllControl") {
+          outgoing.observe = msg[1];
+          this.toClient("observeAllControl", outgoing);
+        }
+
+        if (msg[0] === "observeAllEvents") {
+          outgoing.observe = msg[1];
+          this.toClient("observeAllEvents", outgoing);
+        }
+      }
+    });
+
+    const message = String(
+      `Connected to Collab-Hub Client! You are using ${this.environment}. Send messages to Port ${this.recPort}`
+    );
+    // this.clientOut = dgram.createSocket("udp4");
+    // this.clientOut.connect(this.sendPort, "localhost", (err) => {
+    //   console.log("sending message...");
+    //   this.clientOut.send(message, (err) => {
+    //     // this.clientOut.close();
+    //   });
+    //   // this.clientOut.send(message, 0, message.length);
+    // });
 
     console.log(`loaded client for ${this.name}`);
   }
 
-  toEnv = (options) => {
+  toEnv = (type, options) => {
+    console.log("to env message: " + options);
+    console.dir(options);
 
-  }
+    switch (type) {
+      case MESSAGETYPE.EVENT:
+        this.clientOut.send(options.header);
+        break;
+      case MESSAGETYPE.CONTROL:
+        let msg = options.header + " " + options.values;
+        this.clientOut.send(msg);
+        break;
+    }
+  };
 
-  toServer = (options) => {
+  toServer = (options) => {};
 
-  }
-
-  settings = (options) => {
-
-  }
+  settings = (options) => {};
 }
-
-
 
 ///
 module.exports = {
